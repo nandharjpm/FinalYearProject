453f66ba096b5a2dce3284f0d7bfd35e821d1d1c
nandharjpm <nandharjpm@gmail.com>
2024-04-03 12:36:57 +0530

    Updating the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/components/Footer.scss";
 
 const Footer = () => {
+  const [showIframe, setShowIframe] = useState(false);
+
   return (
     <footer>
+      {/* Button to toggle iframe visibility */}
+      <button onClick={() => setShowIframe(!showIframe)}
+      style={{
+        padding: '10px 20px',
+        backgroundColor: '#4CAF50',
+        margin: '10px 10px 10px 10px',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        fontSize: '16px',
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+        transition: 'background-color 0.3s ease',
+        outline: 'none',
+      }}>
+        {showIframe ? "Hide Chat Bot" : "Show Chat Bot"}
+      </button>
       <div className="webpage-info">
+      {showIframe && (
+        <iframe
+          title="External Content"
+          src="https://poe.com/chat/262p9em5ni2k2symts4"
+          width="600"
+          height="400"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      )}
         <h6 className="brand-name">Sparkles</h6>
         <div className="attribution">
           <p>Images:</p>
@@ -20,7 +49,7 @@ const Footer = () => {
           </p>
         </div>
         <div className="creator">
-          <p>Created by RReiso 2022</p>
+          <p></p>
           <div className="creator-links">
             <a
               className="link"
@@ -28,12 +57,11 @@ const Footer = () => {
               href="https://github.com/rreiso"
               target="_blank"
             >
-              <p className="sr-only">RReiso github.com profile</p>
-              <i className="fa fa-github fa-lg" aria-hidden="true"></i>
             </a>
           </div>
         </div>
       </div>
+      {/* Render iframe based on showIframe state */}
     </footer>
   );
 };
